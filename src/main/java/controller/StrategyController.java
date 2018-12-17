4dea242f705287130223f9a1777dd645c45f8f30
Konrad <konrad1997@gmail.com>
2018-12-17 02:35:57 +0100

    Refactor code

--- src/main/java/controller/StrategyController.java
+++ src/main/java/controller/StrategyController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -14,10 +12,25 @@ public class StrategyController {
     private StrategyList strategyList = new StrategyList();
 
     @FXML
-    private Button strategyDeleteButton;
+    private ListView<String> strategyListView = new ListView<>();
 
     @FXML
-    private Button ruleDeleteButton;
+    private ListView<String> ruleListView = new ListView<>();
+
+    @FXML
+    private Label addStrategyErrorLabel;
+
+    @FXML
+    private Label addRuleErrorLabel;
+
+    @FXML
+    private TextField ruleTextFieldDays = new TextField();
+
+    @FXML
+    private TextField ruleTextFieldValue = new TextField();
+
+    @FXML
+    private TextField strategyTextFieldValue = new TextField();
 
     @FXML
     private RadioButton valueRiseRadioButton;
@@ -38,46 +51,66 @@ public class StrategyController {
     private RadioButton orRadioButton;
 
     @FXML
-    private Label addStrategyErrorLabel;
-
-    @FXML
-    private Label addRuleErrorLabel;
-
-    @FXML
-    private ListView<String> strategyListView = new ListView<>();
-
-    @FXML
-    private ListView<String> ruleListView = new ListView<>();
-
-    @FXML
-    private TextField ruleTextFieldDays = new TextField();
-
-    @FXML
-    private TextField ruleTextFieldValue = new TextField();
-
-    @FXML
-    private TextField strategyTextFieldValue = new TextField();
-
-    @FXML
     private Button ruleSaveButton = new Button();
 
     @FXML
     private Button addStrategyButton = new Button();
 
     @FXML
+    private Button strategyDeleteButton;
+
+    @FXML
+    private Button ruleDeleteButton;
+
+    @FXML
     private void initialize() {
+
+        setTextFieldFormatters();
+        setButtonActionHandlers();
+        configureStrategyListView();
+
+    }
+
+    private void configureStrategyListView() {
+
+        strategyListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (strategyListView.getSelectionModel().getSelectedIndex() != -1)
+                ruleListView.setItems(FXCollections.observableArrayList(
+                        strategyList.getStrategy(strategyListView.getSelectionModel().getSelectedIndex()).getRules()
+                ));
+
+        });
+
+        strategyListView.setItems(FXCollections.observableArrayList(strategyList.getStrategyList()));
+
+    }
+
+    private void setButtonActionHandlers(){
+
+        ruleSaveButton.setOnAction(this::handleRuleSaveButtonClick);
+        addStrategyButton.setOnAction(this::handleAddNewStrategyButtonClick);
+        ruleDeleteButton.setOnAction(this::handleRuleDeleteButtonClick);
+        strategyDeleteButton.setOnAction(this::handleStrategyDeleteButtonClick);
+
+    }
+
+    private void setTextFieldFormatters(){
+
+        String decimalFormatRegex = "(^[1-9]?[0-9](\\.[0-9]*)?$|^$)";
+        String integerFormatRegex = "^[0-9]*";
 
         ruleTextFieldValue.setTextFormatter( new TextFormatter<>(c ->
         {
-            if (c.getControlNewText().matches("(^[1-9]?[0-9](\\.[0-9]*)?$|^$)")){
+            if (c.getControlNewText().matches(decimalFormatRegex)){
                 return c;
-            }else{
+            } else {
                 return null;
             }
         }));
 
         ruleTextFieldDays.setTextFormatter(new TextFormatter<>(c ->{
-            if (c.getControlNewText().matches("^[0-9]*")){
+            if (c.getControlNewText().matches(integerFormatRegex)){
                 return c;
             } else {
                 return null;
@@ -85,69 +118,74 @@ public class StrategyController {
         }));
 
         strategyTextFieldValue.setTextFormatter(new TextFormatter<>(c ->{
-            if (c.getControlNewText().matches("(^[1-9]?[0-9](\\.[0-9]*)?$|^$)")){
+            if (c.getControlNewText().matches(decimalFormatRegex)){
                 return c;
             } else {
                 return null;
             }
         }));
 
-        ruleSaveButton.setOnAction(this::handleRuleSaveButtonClick);
-        addStrategyButton.setOnAction(this::handleAddNewStrategyButtonClick);
-        ruleDeleteButton.setOnAction(this::handleRuleDeleteButtonClick);
-        strategyDeleteButton.setOnAction(this::handleStrategyDeleteButtonClick);
+    }
 
-        strategyListView.setItems(FXCollections.observableArrayList(strategyList.getStrategyList()));
+    private void resetRuleView(){
 
-        strategyListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if (strategyListView.getSelectionModel().getSelectedIndex() != -1)
-                    ruleListView.setItems(FXCollections.observableArrayList(
-                            strategyList.getStrategy(strategyListView.getSelectionModel().getSelectedIndex()).getRules()
-                    ));
-            }
-        });
+        ruleTextFieldDays.clear();
+        ruleTextFieldValue.clear();
+        valueRiseRadioButton.setSelected(true);
+        addRuleErrorLabel.setText("");
+
     }
 
     private void handleRuleSaveButtonClick(Event event) {
         try {
 
             int days = Integer.valueOf(ruleTextFieldDays.getText());
+
             float value = Float.valueOf(ruleTextFieldValue.getText());
             if (valueFallRadioButton.isSelected()) value *= -1;
 
-            strategyList.getStrategy(strategyListView.getSelectionModel().getSelectedIndex())
-                    .addRule(new Rule(days, value));
+            Strategy selectedStrategy = strategyList.getStrategy(strategyListView.getSelectionModel().getSelectedIndex());
+            selectedStrategy.addRule(new Rule(days, value));
 
-            ruleListView.setItems(FXCollections.observableArrayList(
-                    strategyList.getStrategy(strategyListView.getSelectionModel().getSelectedIndex()).getRules()
-            ));
+            ruleListView.setItems(FXCollections.observableArrayList(selectedStrategy.getRules()));
 
-            ruleTextFieldDays.clear();
-            ruleTextFieldValue.clear();
-            valueRiseRadioButton.setSelected(true);
-            addRuleErrorLabel.setText("");
+            resetRuleView();
+
         } catch (ArrayIndexOutOfBoundsException e){
+
             addRuleErrorLabel.setText("Select strategy first to add rule to it");
+
         } catch (NumberFormatException e){
+
             addRuleErrorLabel.setText("Both text fields cannot be null");
+
         }
+    }
+
+    private void resetStrategyView(){
+
+        strategyTextFieldValue.clear();
+        buyRadioButton.setSelected(true);
+        andRadioButton.setSelected(true);
+        addStrategyErrorLabel.setText("");
+
     }
 
     private void handleAddNewStrategyButtonClick(Event event) {
         try {
+
             float percentage = Float.valueOf(strategyTextFieldValue.getText());
             if (sellRadioButton.isSelected()) percentage *= -1;
 
             strategyList.addStrategy(new Strategy(percentage, andRadioButton.isSelected()));
             strategyListView.setItems(FXCollections.observableArrayList(strategyList.getStrategyList()));
 
-            strategyTextFieldValue.clear();
-            buyRadioButton.setSelected(true);
-            andRadioButton.setSelected(true);
-            addStrategyErrorLabel.setText("");
+            resetStrategyView();
+
         } catch (NumberFormatException e){
+
             addStrategyErrorLabel.setText("Invalid buy/sell value");
+
         }
     }
 
