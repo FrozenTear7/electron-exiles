550c6b634c2ac4a6f67ab948d2a181d46744e95b
Fafruch <fafruszek@gmail.com>
2018-12-19 00:59:02 +0100

    Fix DataRowList tests, refactor DataRowList

--- src/test/java/DataRowListTest.java
+++ src/test/java/DataRowListTest.java
@@ -1,4 +1,4 @@
-/*
+import exceptions.LoadException;
 import model.DataRow;
 import model.DataRowList;
 import org.junit.jupiter.api.BeforeAll;
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,11 +18,11 @@ class DataRowListTest {
     private static DataRow thirdDataRow;
 
     @BeforeAll
-    static void setUp() throws ParseException {
-        final Date firstDate = new SimpleDateFormat("yyyy-MM-dd").parse("1984-09-07");
-        final Date secondDate = new SimpleDateFormat("yyyy-MM-dd").parse("1994-09-07");
-        final Date thirdDate = new SimpleDateFormat("yyyy-MM-dd").parse("2004-09-07");
-        final Float stockValue = (float) 0.0;
+    static void setUp() throws LoadException {
+        final String firstDate = "1974-09-07";
+        final String secondDate = "1984-09-07";
+        final String thirdDate = "1994-09-07";
+        final String stockValue = "0.1";
 
         firstDataRow = new DataRow(firstDate, stockValue);
         secondDataRow = new DataRow(secondDate, stockValue);
@@ -40,12 +37,10 @@ class DataRowListTest {
         dl.addRow(firstDataRow);
         dl.addRow(secondDataRow);
 
-        dl.sortListByDate();
-
         List<DataRow> sortedRows = dl.getDataRowList();
 
         assertEquals(firstDataRow, sortedRows.get(0));
         assertEquals(secondDataRow, sortedRows.get(1));
         assertEquals(thirdDataRow, sortedRows.get(2));
     }
-}*/
+}
